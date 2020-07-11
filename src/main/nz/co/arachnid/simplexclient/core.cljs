7fa5d3f9550fd2dd09af6197f00ed518e2b53437
Nick Jones <nick.jones@arachnid.co.nz>
2020-07-11 12:55:11 +1200

    Configuring assets path.

--- src/main/nz/co/arachnid/simplexclient/core.cljs
+++ src/main/nz/co/arachnid/simplexclient/core.cljs
@@ -1,1 +1,10 @@
-(ns nz.co.arachnid.simplexclient.core)
+(ns nz.co.arachnid.simplexclient.core
+  (:require [rum.core :as rum]))
+
+(def counter-atom (atom 0))
+
+(rum/defc counter < rum/reactive []
+  [:div { :on-click (fn [_] (swap! counter-atom inc)) }
+   "Clicks: " (rum/react counter)])
+
+(rum/mount (counter-atom) js/document.body)
