c28aeb82f74387d377d8bf6378a24c20a8eea930
Nick Jones <nick.jones@arachnid.co.nz>
2020-07-11 16:37:05 +1200

    Getting the initial compilation working.

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # Basic Compilation And Setup
 
+## Straight Compilation
+
 ```bash
 npx shadow-cljs compile app
 ```
+
+## Watch With NREPL And Hot Reloading
+
+```bash
+npx shadow-cljs watch app
+```
+
+# General Notes
+
+ - public/index.html is under version control and is safe to edit.
+ - public/js is the compiler target directory.
--- src/main/nz/co/arachnid/simplexclient/core.cljs
+++ src/main/nz/co/arachnid/simplexclient/core.cljs
@@ -4,7 +4,21 @@
 (def counter-atom (atom 0))
 
 (rum/defc counter < rum/reactive []
-  [:div { :on-click (fn [_] (swap! counter-atom inc)) }
-   "Clicks: " (rum/react counter)])
+  [:div { :on-click (fn [_] (swap! counter-atom inc))}
+   "Clicks: " (rum/react counter-atom)])
 
-(rum/mount (counter-atom) js/document.body)
+;; =================================
+;; Entry Hooks For Stop/Start/Reload
+;; =================================
+
+(defn ^:dev/before-load stop []
+  (js/console.log "stop"))
+
+(defn ^:dev/after-load start []
+  (js/console.log "start")
+  (rum/mount (counter) js/document.body))
+
+(defn ^:export init []
+  "Entry point for application, referenced in index.html"
+  (js/console.log "Initializing Simplex Client Init")
+  (rum/mount (counter) js/document.body))
